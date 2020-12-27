4e114576f3380e047c943f1c53b32e63b5169ebf
Pedro Henrique <p13dr0h@gmail.com>
2020-12-27 01:49:18 -0300

    refactor : move api details from domain class to data class

--- lib/data/usecases/remote_authentication.dart
+++ lib/data/usecases/remote_authentication.dart
@@ -5,13 +5,29 @@ import '../../domain/usecases/usecases.dart';
 import '../http/http.dart';
 
 class RemoteAuthentication {
-	final HttpClient httpClient;
-	final String url;
-	
-	RemoteAuthentication({@required this.httpClient, @required this.url});
-	
-	Future<void> auth(AuthenticationParams authenticationParams) async {
-		//final Map body = {'email':authenticationParams.email, 'password':authenticationParams.secret};
-		await httpClient.request(url: url, method: 'post', body:authenticationParams.toJson());
-	}
+  final HttpClient httpClient;
+  final String url;
+
+  RemoteAuthentication({@required this.httpClient, @required this.url});
+
+  Future<void> auth(AuthenticationParams authenticationParams) async {
+    //final Map body = {'email':authenticationParams.email, 'password':authenticationParams.secret};
+    await httpClient.request(
+        url: url,
+        method: 'post',
+        body: RemoteAuthenticationParams.fromDomain(authenticationParams)
+            .toJson());
+  }
+}
+
+class RemoteAuthenticationParams {
+  final String email;
+  final String password;
+
+  RemoteAuthenticationParams({@required this.email, @required this.password});
+
+  factory RemoteAuthenticationParams.fromDomain(AuthenticationParams entity) =>
+      RemoteAuthenticationParams(email: entity.email, password: entity.secret);
+
+  Map toJson() => {'email': email, 'password': password};
 }
