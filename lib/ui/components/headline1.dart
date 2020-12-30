447ecd3e9b9da590b3a09391f0b0729ead392b2b
Pedro Henrique <p13dr0h@gmail.com>
2020-12-30 02:49:25 -0300

    login page improved

---
+++ lib/ui/components/headline1.dart
@@ -0,0 +1,20 @@
+
+import 'package:flutter/material.dart';
+
+class Headline1 extends StatelessWidget {
+	final String text;
+	
+	const Headline1({
+		Key key,
+		@required this.text
+	}) : super(key: key);
+	
+	@override
+	Widget build(BuildContext context) {
+		return Text(
+			"$text".toUpperCase(),
+			textAlign: TextAlign.center,
+			style: Theme.of(context).textTheme.headline1,
+		);
+	}
+}
