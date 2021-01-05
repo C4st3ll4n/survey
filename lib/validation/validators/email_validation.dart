8a66700005df8af26cf331e67f2c2f6d25f8e6a0
Pedro Henrique <p13dr0h@gmail.com>
2021-01-05 18:16:42 -0300

    email validation fixed

--- lib/validation/validators/email_validation.dart
+++ lib/validation/validators/email_validation.dart
@@ -1,14 +1,14 @@
 import '../protocols/protocols.dart';
 
 class EmailValidation implements FieldValidation {
-	EmailValidation(this.field);
-	
-	final String field;
-	
-	@override
-	String validate(String value) {
-		final RegExp regExp = RegExp(r"^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/");
-		final isValid = value?.isNotEmpty != true || regExp.hasMatch(value)? true:false;
-		return !isValid?null:"Campo inválido.";
-	}
+  EmailValidation(this.field);
+
+  final String field;
+
+  @override
+  String validate(String value) {
+    final RegExp regExp = RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+");
+    final isValid = value?.isNotEmpty != true || regExp.hasMatch(value);
+    return isValid ? null : "Campo inválido.";
+  }
 }
