97becde84cd442582375f74f5dcb1b4f13650f44
Pedro Henrique <p13dr0h@gmail.com>
2021-01-06 15:09:31 -0300

    ensure LocalSaveCurrentAccount throws UnexpectedError SaveSecureCache with null value

--- test/data/usecases/save_current_account/local_save_current_account_test.dart
+++ test/data/usecases/save_current_account/local_save_current_account_test.dart
@@ -3,19 +3,20 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
 import 'package:survey/domain/entities/entities.dart';
+import 'package:survey/domain/helpers/domain_error.dart';
 import 'package:survey/domain/usecases/usecases.dart';
 
-class SaveCacheStorageSpy extends Mock implements SaveSecureCacheStorage{}
+class SaveCacheStorageSpy extends Mock implements SaveSecureCacheStorage {}
 
 abstract class SaveSecureCacheStorage {
-	Future<void> saveSecure({String key, String value});
+  Future<void> saveSecure({String key, String value});
 }
 
 void main() {
   LocalSaveCurrentAccount sut;
   AccountEntity account;
   SaveSecureCacheStorage secureCache;
-  
+
   setUp(() {
     account = AccountEntity(faker.guid.guid());
     secureCache = SaveCacheStorageSpy();
@@ -23,21 +24,42 @@ void main() {
   });
 
   test(
-    "Should call SaveCacheStorage",
+    "Should call SaveSecureCacheStorage with correct values",
     () async {
-    	
       await sut.save(account);
-      verify(secureCache.saveSecure(key:'token',value:account.token));
+      verify(secureCache.saveSecure(key: 'token', value: account.token));
+    },
+  );
+
+  test(
+    "Should throw unexpected error if SaveSecureCacheStorage",
+    () async {
+      when(
+        secureCache.saveSecure(
+          key: anyNamed("key"),
+          value: anyNamed("value"),
+        ),
+      ).thenThrow(Exception());
+
+      final future = sut.save(account);
+
+      //verify(secureCache.saveSecure(key:'token',value:account.token));
+      expect(future, throwsA(DomainError.unexpected));
     },
   );
 }
 
 class LocalSaveCurrentAccount implements SaveCurrentAccount {
-	final SaveSecureCacheStorage saveSecureCacheStorage;
+  final SaveSecureCacheStorage saveSecureCacheStorage;
 
   LocalSaveCurrentAccount({this.saveSecureCacheStorage});
+
   @override
   Future<void> save(AccountEntity account) async {
-    await saveSecureCacheStorage.saveSecure(key: "token",value: account.token);
+    try{
+      await saveSecureCacheStorage.saveSecure(key: "token", value: account.token);
+    }catch(e){
+      throw DomainError.unexpected;
+    }
   }
 }
