4980b7ec3ad6628b6cc0ee32f399fec14507ae65
Pedro Henrique <p13dr0h@gmail.com>
2021-01-06 03:02:47 -0300

    organized data usecase folder

--- lib/data/usecases/authentication/remote_authentication.dart
+++ lib/data/usecases/authentication/remote_authentication.dart
@@ -1,11 +1,11 @@
 import 'package:meta/meta.dart';
 import 'package:survey/data/models/remote_account_model.dart';
 
-import '../../domain/helpers/helpers.dart';
-import '../../domain/usecases/usecases.dart';
-import '../../domain/entities/entities.dart';
+import '../../../domain/helpers/helpers.dart';
+import '../../../domain/usecases/usecases.dart';
+import '../../../domain/entities/entities.dart';
 
-import '../http/http.dart';
+import '../../http/http.dart';
 
 class RemoteAuthentication implements Authentication{
   final HttpClient httpClient;
