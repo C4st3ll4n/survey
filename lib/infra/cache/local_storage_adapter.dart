7c7c6dedbf06eab74a6cfac85f51d4f1c85e894e
Pedro Henrique <p13dr0h@gmail.com>
2021-01-19 00:00:48 -0300

    ensure local storage adapter calls fetch secure with correct value

--- lib/infra/cache/local_storage_adapter.dart
+++ lib/infra/cache/local_storage_adapter.dart
@@ -14,4 +14,8 @@ class LocalStorageAdapter implements SaveSecureCacheStorage {
 			{@required String key, @required String value}) async {
 		await secureStorage.write(key: key, value: value);
 	}
+
+  Future<void> fetchSecure(String key) async{
+		await secureStorage.read(key: key);
+  }
 }
