c34c5a643ece47d960807b3b20c43bd4734499be
Pedro Henrique <p13dr0h@gmail.com>
2021-01-19 02:15:59 -0300

    moved production code to their onw files

---
+++ lib/ui/pages/splash/splash_page.dart
@@ -0,0 +1,37 @@
+
+import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
+
+import 'splash_presenter.dart';
+
+class SplashPage extends StatelessWidget {
+	const SplashPage({Key key, @required this.presenter}) : super(key: key);
+	
+	final SplashPresenter presenter;
+	
+	@override
+	Widget build(BuildContext context) {
+		presenter.loadCurrentAccount();
+		return Scaffold(
+			appBar: AppBar(
+				title: Text("Surveys"),
+				centerTitle: true,
+			),
+			body: Builder(
+				builder: (BuildContext context) {
+					presenter.navigateToStream.listen(
+								(page) {
+							if (page?.isNotEmpty == true) {
+								Get.offAllNamed(page);
+							}
+						},
+					);
+					
+					return Center(
+						child: CircularProgressIndicator(),
+					);
+				},
+			),
+		);
+	}
+}
