34794e29c83ce4b1a284ad25a2aa51b4af525ad6
Pedro Henrique <p13dr0h@gmail.com>
2021-01-21 15:50:26 -0300

    ensure emailInUse message is called

--- lib/ui/helpers/errors/ui_error.dart
+++ lib/ui/helpers/errors/ui_error.dart
@@ -1,27 +1,33 @@
 import '../helpers.dart';
 
-enum UIError{
-	unexpected, invalidCredentials,
-	requiredField, invalidField
+enum UIError {
+	unexpected,
+	invalidCredentials,
+	requiredField,
+	invalidField,
+	emailInUse
 }
 
 extension UIErrorExtension on UIError{
-	String get description{
-		switch(this){
-			
-		  case UIError.unexpected:
-		    return R.strings.msgUnexpectedError;
-		    break;
-		  case UIError.invalidCredentials:
-		    return R.strings.msgInvalidCredentials;
-		    break;
+	String get description {
+		switch (this) {
+			case UIError.emailInUse:
+				return R.strings.msgEmailInUse;
+				break;
+			case UIError.unexpected:
+				return R.strings.msgUnexpectedError;
+				break;
+			case UIError.invalidCredentials:
+				return R.strings.msgInvalidCredentials;
+				break;
 			case UIError.invalidField:
 				return "Campo inválido.";
 				break;
 			case UIError.requiredField:
 				return "Campo obrigatório.";
 				break;
-			default: return "Algo errado aconteceu. Tente novamente.";
+			default:
+				return "Algo errado aconteceu. Tente novamente.";
 		}
 	}
 }
