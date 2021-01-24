9983157d7e0da845dd04020e839297d6b8dbd4cc
Pedro Henrique <p13dr0h@gmail.com>
2021-01-24 18:42:34 -0300

    fixed imports

--- test/data/usecases/load_surveys/local_load_surveys_test.dart
+++ test/data/usecases/load_surveys/local_load_surveys_test.dart
@@ -1,6 +1,16 @@
 import 'package:flutter_test/flutter_test.dart';
+import 'package:survey/data/usecases/load_surveys/load_surveys.dart';
 
 void main(){
+	
+	LocalLoadSurveys sut;
+	List<Map> listData;
+	
+	
+	setUp((){
+	
+	});
+	
 	test("",(){
 	
 	});
