f40fe0a7d33e63238b6f927c64596204250558be
Pedro Henrique <p13dr0h@gmail.com>
2021-01-23 23:36:37 -0300

    ensure signup page calls the correct validations

--- test/main/factories/pages/signup/signup_validation_factory_test.dart
+++ test/main/factories/pages/signup/signup_validation_factory_test.dart
@@ -2,15 +2,25 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:survey/main/factories/factories.dart';
 import 'package:survey/validation/validators/validators.dart';
 
-void main(){
-	
-	test("Should return the correct validations",(){
-		final validations = makeLoginValidations();
-		expect(validations, [
-			RequiredFieldValidation("email"),
-			EmailValidation("email"),
-			RequiredFieldValidation("password"),
-			MinLengthFieldValidation(field: "password", minLength: 3)
-		]);
-	});
+void main() {
+  test(
+    "Should return the correct validations",
+    () {
+      final validations = makeSignUpValidations();
+      expect(
+        validations,
+        [
+          RequiredFieldValidation("name"),
+          MinLengthFieldValidation(field: "name", minLength: 3),
+          RequiredFieldValidation("email"),
+          EmailValidation("email"),
+          RequiredFieldValidation("password"),
+          MinLengthFieldValidation(field: "password", minLength: 3),
+          RequiredFieldValidation("passwordConfirmation"),
+          CompareFieldValidation(
+              field: "passwordConfirmation", fieldToCompare: "password"),
+        ],
+      );
+    },
+  );
 }
