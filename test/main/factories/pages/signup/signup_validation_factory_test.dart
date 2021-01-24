33082016bd95caa74ea76800cdca0120b3df8b30
Pedro Henrique <p13dr0h@gmail.com>
2021-01-23 22:29:12 -0300

    fixed the signup factory validation test

--- test/main/factories/pages/signup/signup_validation_factory_test.dart
+++ test/main/factories/pages/signup/signup_validation_factory_test.dart
@@ -2,17 +2,15 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:survey/main/factories/factories.dart';
 import 'package:survey/validation/validators/validators.dart';
 
-
-
-
 void main(){
-	
 	
 	test("Should return the correct validations",(){
 		final validations = makeLoginValidations();
 		expect(validations, [
 			RequiredFieldValidation("email"),
 			EmailValidation("email"),
-			RequiredFieldValidation("password"),]);
+			RequiredFieldValidation("password"),
+			MinLengthFieldValidation(field: "password", minLength: 3)
+		]);
 	});
 }
