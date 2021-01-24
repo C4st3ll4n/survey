48ae87bec8a309acd8ce517e620488fb9be66585
Pedro Henrique <p13dr0h@gmail.com>
2021-01-24 17:09:42 -0300

    ensure remoteLoadSurveys throws an error on API invalid data

--- lib/data/usecases/load_surveys/remote_load_survey.dart
+++ lib/data/usecases/load_surveys/remote_load_survey.dart
@@ -2,6 +2,7 @@ import 'package:meta/meta.dart';
 import 'package:survey/data/http/http.dart';
 import 'package:survey/data/models/models.dart';
 import 'package:survey/domain/entities/survey_entity.dart';
+import 'package:survey/domain/helpers/domain_error.dart';
 import 'package:survey/domain/usecases/usecases.dart';
 
 class RemoteLoadSurveys implements LoadSurveys {
@@ -12,9 +13,15 @@ class RemoteLoadSurveys implements LoadSurveys {
 
   @override
   Future<List<SurveyEntity>> load() async {
+    try{
+    
     final response = await httpClient.request(url: url, method: 'get');
     return response
         .map((json) => RemoteSurveyModel.fromJson(json).toEntity())
         .toList();
+    }
+    on HttpError{
+      throw DomainError.unexpected;
+    }
   }
 }
