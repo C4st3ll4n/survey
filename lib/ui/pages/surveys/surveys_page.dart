1bafc8d74fc5ad9291ecbdad0abaf7ae16ffd72e
Pedro Henrique <p13dr0h@gmail.com>
2021-01-26 09:48:02 -0300

    ensure surveysPage handles loading correctly

--- lib/ui/pages/surveys/surveys_page.dart
+++ lib/ui/pages/surveys/surveys_page.dart
@@ -1,6 +1,7 @@
 import 'package:faker/faker.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
+import 'package:survey/ui/components/components.dart';
 import 'package:survey/ui/helpers/helpers.dart';
 import 'package:survey/ui/pages/surveys/surveys_presenter.dart';
 import '../../../data/models/models.dart';
@@ -19,21 +20,52 @@ class SurveysPage extends StatelessWidget {
         centerTitle: true,
         title: Text(R.strings.surveys),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(vertical:20.0),
-        child: CarouselSlider(
-          items: [
-            SurveyItem(survey: RemoteSurveyModel.fromJson({
-              "id": faker.guid.guid(),
-              "question": faker.randomGenerator.string(50),
-              "didAnswer": faker.randomGenerator.boolean(),
-              "dateTime": faker.date.dateTime().toIso8601String()
-            }).toEntity(),)
-          ],
-          options: CarouselOptions(
-            enlargeCenterPage: true,aspectRatio: 1
+      body: Builder(
+        builder:(contexto){
+  
+          presenter.isLoadingStream.listen(
+                (isLoading) {
+              if (isLoading == true) {
+                showSimpleLoading(contexto);
+              } else {
+                hideLoading(contexto);
+              }
+            },
+          );
+  
+         /* presenter.mainErrorStream.listen(
+                  (UIError error) {
+                if (error != null) {
+                  showErrorMessage(contexto, error.description);
+                }
+              }
+          );
+  
+          presenter.navigateToStream.listen(
+                  (page) {
+                if (page != null && page.trim().isNotEmpty) {
+                  Get.offAllNamed(page);
+                }
+              }
+          );*/
+          
+          return Padding(
+          padding: const EdgeInsets.symmetric(vertical:20.0),
+          child: CarouselSlider(
+            items: [
+              SurveyItem(survey: RemoteSurveyModel.fromJson({
+                "id": faker.guid.guid(),
+                "question": faker.randomGenerator.string(50),
+                "didAnswer": faker.randomGenerator.boolean(),
+                "dateTime": faker.date.dateTime().toIso8601String()
+              }).toEntity(),)
+            ],
+            options: CarouselOptions(
+              enlargeCenterPage: true,aspectRatio: 1
+            ),
           ),
-        ),
+        );
+        },
       ),
     );
   }
--- test/ui/page/surveys_page_test.dart
+++ test/ui/page/surveys_page_test.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:get/get.dart';
 import 'package:mockito/mockito.dart';
@@ -5,24 +8,67 @@ import 'package:survey/ui/pages/pages.dart';
 
 void main() {
   SurveysPresenter presenter;
+  StreamController<bool> isLoadingController;
+  
+  
+  void _initStream(){
+    isLoadingController = StreamController();
+  }
+  void _mockStream(){
+    when(presenter.isLoadingStream).thenAnswer((realInvocation) => isLoadingController.stream);
+  }
+  void _closeStream(){
+    isLoadingController.close();
+  }
+  
+  tearDown((){
+    _closeStream();
+  });
+  
   Future loadPage(WidgetTester tester) async {
     presenter = SurveysPresenterSpy();
+    _initStream();
+    _mockStream();
     final surveysPage = GetMaterialApp(
       initialRoute: "/surveys",
       getPages: [
-        GetPage(name: "/surveys", page: () => SurveysPage(presenter: presenter,)),
+        GetPage(
+            name: "/surveys",
+            page: () => SurveysPage(
+                  presenter: presenter,
+                )),
       ],
     );
 
     await tester.pumpWidget(surveysPage);
   }
 
-  testWidgets(
-      "Should call LoadSurveys on page load", (WidgetTester tester) async {
-        await loadPage(tester);
-        
-        verify(presenter.loadData()).called(1);
+  testWidgets("Should call LoadSurveys on page load",
+      (WidgetTester tester) async {
+    await loadPage(tester);
+
+    verify(presenter.loadData()).called(1);
+  });
+  
+  testWidgets("Should handle loading", (WidgetTester tester) async {
+    await loadPage(tester);
+    
+    isLoadingController.add(true);
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+    
+    isLoadingController.add(false);
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsNothing);
+
+    isLoadingController.add(true);
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsOneWidget);
+
+    isLoadingController.add(null);
+    await tester.pump();
+    expect(find.byType(CircularProgressIndicator), findsNothing);
   });
 }
 
-class SurveysPresenterSpy extends Mock implements SurveysPresenter{}
+class SurveysPresenterSpy extends Mock implements SurveysPresenter {}
