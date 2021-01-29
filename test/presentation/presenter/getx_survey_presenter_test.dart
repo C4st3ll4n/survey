2bdc9943a882f54b463fa921fdfa50f4c5e96232
Pedro Henrique <p13dr0h@gmail.com>
2021-01-28 21:43:22 -0300

    mocked getxSurveysPresenter and ensured that the calls LoadSurvey on LoadData

---
+++ test/presentation/presenter/getx_survey_presenter_test.dart
@@ -0,0 +1,97 @@
+import 'package:faker/faker.dart';
+import 'package:get/get.dart';
+import 'package:meta/meta.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:survey/domain/entities/entities.dart';
+import 'package:survey/domain/helpers/domain_error.dart';
+import 'package:survey/domain/usecases/usecases.dart';
+import 'package:survey/ui/pages/pages.dart';
+
+class LoadSurveysSpy extends Mock implements LoadSurveys {}
+
+void main() {
+  SurveysPresenter sut;
+  LoadSurveys loadSurveys;
+
+  List<SurveyEntity> makeSurveys() => [
+        SurveyEntity(
+            id: "1",
+            question: "Question 1",
+            dateTime: faker.date.dateTime(),
+            didAnswer: true),
+        SurveyEntity(
+            id: "2",
+            question: "Question 2",
+            dateTime: faker.date.dateTime(),
+            didAnswer: false),
+      ];
+
+  PostExpectation _mockLoadSurveysCall() => when(
+        loadSurveys.load(),
+      );
+
+  void mockLoadsurvey() =>
+      _mockLoadSurveysCall().thenAnswer((_) async => makeSurveys());
+
+  test("Should call loadSurveys on LoadData", () async {
+    await sut.loadData();
+    verify(loadSurveys.load()).called(1);
+  });
+
+  setUp(() {
+    loadSurveys = LoadSurveysSpy();
+    sut = GetXSurveysPresenter(loadSurveys: loadSurveys);
+    mockLoadsurvey();
+  });
+}
+
+class GetXSurveysPresenter extends GetxController implements SurveysPresenter {
+  GetXSurveysPresenter({@required this.loadSurveys});
+
+  final LoadSurveys loadSurveys;
+
+  var _isLoading = false.obs;
+  var _dataStream = RxList<SurveyViewModel>();
+
+  @override
+  Stream<bool> get isLoadingStream => _isLoading.stream.distinct();
+
+  @override
+  Stream<List<SurveyViewModel>> get loadSurveysStream =>
+      _dataStream.stream.distinct();
+
+  @override
+  Future<void> loadData() async {
+    _isLoading.value = true;
+    try {
+      final result = await loadSurveys.load();
+      _dataStream.addAll(result
+          .map(
+            (SurveyEntity e) => SurveyViewModel(
+                id: e.id,
+                question: e.question,
+                formatedDate: e.dateTime.toString(), //FIXME
+                didAnswer: e.didAnswer),
+          )
+          .toList());
+    } on DomainError catch (error) {
+      switch (error) {
+        case DomainError.unexpected:
+          // TODO: Handle this case.
+          break;
+        case DomainError.invalidCredentials:
+          // TODO: Handle this case.
+          break;
+        case DomainError.emailInUse:
+          // TODO: Handle this case.
+          break;
+        case DomainError.accessDenied:
+          // TODO: Handle this case.
+          break;
+      }
+    } finally {
+      _isLoading.value = false;
+    }
+  }
+}
