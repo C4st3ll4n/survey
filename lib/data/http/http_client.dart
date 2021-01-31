ed70f4b0d4293e3c5434313296f4b7ecae3135dc
Pedro Henrique <p13dr0h@gmail.com>
2021-01-31 20:00:31 -0300

    adjusted the http client to dynamic

--- lib/data/http/http_client.dart
+++ lib/data/http/http_client.dart
@@ -1,5 +1,5 @@
 import 'package:meta/meta.dart';
 
-abstract class HttpClient<ResponseType> {
-	Future<ResponseType> request({@required String url, @required String method, Map body, Map headers});
+abstract class HttpClient {
+	Future<dynamic> request({@required String url, @required String method, Map body, Map headers});
 }
