21043c870bf9a416493db11b647175b75b8015d8
Pedro Henrique <p13dr0h@gmail.com>
2021-02-01 02:41:53 -0300

    ensure localLoadSurveys calls fetch with correct key

--- lib/data/usecases/load_surveys/local_load_survey.dart
+++ lib/data/usecases/load_surveys/local_load_survey.dart
@@ -1,9 +1,16 @@
+import 'package:meta/meta.dart';
+import '../../cache/cache.dart';
 import '../../../domain/entities/survey_entity.dart';
 import '../../../domain/usecases/usecases.dart';
 
 class LocalLoadSurveys implements LoadSurveys {
+  final FetchCacheStorage fetchCacheStorage;
+
+  LocalLoadSurveys({@required this.fetchCacheStorage});
+
   @override
   Future<List<SurveyEntity>> load() {
+    fetchCacheStorage.fetch("surveys");
   }
 	
 }
