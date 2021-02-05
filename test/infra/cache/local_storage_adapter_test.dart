04e5b36eb83d0b4473074586dbe85598223bb75f
Pedro Henrique <p13dr0h@gmail.com>
2021-02-04 21:41:20 -0300

    added the test to the fetch method on local storage adapter

--- test/infra/cache/local_storage_adapter_test.dart
+++ test/infra/cache/local_storage_adapter_test.dart
@@ -5,52 +5,53 @@ import 'package:mockito/mockito.dart';
 import 'package:survey/infra/cache/cache.dart';
 
 void main() {
-  
   LocalStorageAdapter sut;
   LocalStorage localStorageSpy;
   String key;
   dynamic value;
 
+  setUp(() {
+    key = faker.randomGenerator.string(18);
+    value = faker.randomGenerator.string(18);
+    localStorageSpy = LocalStorageSpy();
+    sut = LocalStorageAdapter(localStorageSpy);
+  });
 
-  group("save", (){
-  
-    void _mockDeleteError() {
-      when(localStorageSpy.deleteItem(any)).thenThrow(Exception());
-    }
-  
-    void _mockSaveError() {
-      when(
-        localStorageSpy.setItem(
-          any,
-          any,
-        ),
-      ).thenThrow(Exception());
-    }
-  
-    setUp(() {
-      key = faker.randomGenerator.string(18);
-      value = faker.randomGenerator.string(18);
-      localStorageSpy = LocalStorageSpy();
-      sut = LocalStorageAdapter(localStorageSpy);
-    });
-  
+  void _mockDeleteError() {
+    when(localStorageSpy.deleteItem(any)).thenThrow(Exception());
+  }
+
+  void _mockFetchError() {
+    when(localStorageSpy.getItem(any)).thenThrow(Exception());
+  }
+
+  void _mockSaveError() {
+    when(
+      localStorageSpy.setItem(
+        any,
+        any,
+      ),
+    ).thenThrow(Exception());
+  }
+
+  group("save", () {
     test("Ensure calls save with correct values", () async {
       await sut.save(key: key, value: value);
       verify(localStorageSpy.deleteItem(key)).called(1);
       verify(localStorageSpy.setItem(key, value)).called(1);
     });
-  
+
     test("Ensure calls delete with correct values", () async {
-      await sut.save(key:key, value: value);
+      await sut.save(key: key, value: value);
       verify(localStorageSpy.deleteItem(key)).called(1);
     });
-  
+
     test("Should throw if DeleteItem throw", () async {
       _mockDeleteError();
       final future = sut.save(key: key, value: value);
       expect(future, throwsA(Exception));
     });
-  
+
     test("Should throw if SaveItem throw", () async {
       _mockSaveError();
       final future = sut.save(key: key, value: value);
@@ -58,12 +59,7 @@ void main() {
     });
   });
 
-  group("delete", (){
-  
-    void _mockDeleteError() {
-      when(localStorageSpy.deleteItem(any)).thenThrow(Exception());
-    }
-  
+  group("delete", () {
     void _mockSaveError() {
       when(
         localStorageSpy.setItem(
@@ -72,28 +68,47 @@ void main() {
         ),
       ).thenThrow(Exception());
     }
-  
-    setUp(() {
-      key = faker.randomGenerator.string(18);
-      value = faker.randomGenerator.string(18);
-      localStorageSpy = LocalStorageSpy();
-      sut = LocalStorageAdapter(localStorageSpy);
-    });
-  
+
     test("Ensure calls delete with correct values", () async {
       await sut.delete(key);
       verify(localStorageSpy.deleteItem(key)).called(1);
     });
-  
+
     test("Should throw if DeleteItem throw", () async {
       _mockDeleteError();
       final future = sut.delete(key);
       expect(future, throwsA(Exception));
     });
-  
   });
-  
-  
+
+  group("fetch", () {
+    
+    String result;
+    void _mockFetchSuccess() {
+      when(localStorageSpy.getItem(any)).thenAnswer((_) async => result);
+    }
+
+    setUp(() {
+      _mockFetchSuccess();
+    });
+
+    test("Ensure calls fetch with correct values", () async {
+      await sut.fetch(key);
+      verify(localStorageSpy.getItem(key)).called(1);
+    });
+
+    test("Ensure returns same value as LocalStorage", () async {
+      _mockFetchSuccess();
+      final data = await sut.fetch(key);
+      expect(data, result);
+    });
+
+    test("Should throw if Fetch throw", () async {
+      _mockFetchError();
+      final future = sut.fetch(key);
+      expect(future, throwsA(Exception));
+    });
+  });
 }
 
 class LocalStorageSpy extends Mock implements LocalStorage {}
