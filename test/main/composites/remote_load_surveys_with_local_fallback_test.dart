62bd46df0ab468f459c636dc5bfe5dda6dbbb8a4
Pedro Henrique <p13dr0h@gmail.com>
2021-02-05 23:07:43 -0300

    ensure composite calls Local save with remote data

--- test/main/composites/remote_load_surveys_with_local_fallback_test.dart
+++ test/main/composites/remote_load_surveys_with_local_fallback_test.dart
@@ -1,3 +1,4 @@
+import 'package:faker/faker.dart';
 import 'package:meta/meta.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
@@ -8,27 +9,54 @@ import 'package:survey/domain/usecases/usecases.dart';
 void main() {
   RemoteLoadSurveysWithLocalFallback sut;
   RemoteLoadSurveys remote;
-  
+  LocalLoadSurveys local;
+  List<SurveyEntity> surveys;
+  List<SurveyEntity> _mockValidSurveys() => [
+        SurveyEntity(
+            id: faker.guid.guid(),
+            question: faker.randomGenerator.string(50),
+            dateTime: faker.date.dateTime(),
+            didAnswer: true)
+      ];
+
+  void mockRemoteLoad() {
+    surveys = _mockValidSurveys();
+    when(remote.load()).thenAnswer((_) async => surveys);
+  }
+
   setUp(() {
-    remote  = RemoteLoadSurveysSpy();
-    sut = RemoteLoadSurveysWithLocalFallback(remote: remote);
+    remote = RemoteLoadSurveysSpy();
+    local = LocalLoadSurveysSpy();
+    sut = RemoteLoadSurveysWithLocalFallback(remote: remote, local: local);
+    mockRemoteLoad();
   });
 
   test("Should call remote load", () async {
     await sut.load();
     verify(remote.load()).called(1);
   });
+  test("Should call local save with remote data", () async {
+    await sut.load();
+    verify(local.save(surveys)).called(1);
+  });
 }
 
 class RemoteLoadSurveysSpy extends Mock implements RemoteLoadSurveys {}
 
+class LocalLoadSurveysSpy extends Mock implements LocalLoadSurveys {}
+
 class RemoteLoadSurveysWithLocalFallback implements LoadSurveys {
-  RemoteLoadSurveysWithLocalFallback({@required this.remote});
-  
+  RemoteLoadSurveysWithLocalFallback({
+    @required this.remote,
+    @required this.local,
+  });
+
   final RemoteLoadSurveys remote;
+  final LocalLoadSurveys local;
 
   @override
   Future<List<SurveyEntity>> load() async {
-    await remote.load();
+    final data = await remote.load();
+    await local.save(data);
   }
 }
