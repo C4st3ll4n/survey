63ff979f5d1d1a5dc95e9ca72b6a45530bc0ba58
Pedro Henrique <p13dr0h@gmail.com>
2021-02-06 15:57:04 -0300

    added factories

--- lib/main/factories/pages/surveys/load_surveys_factory.dart
+++ lib/main/factories/pages/surveys/load_surveys_factory.dart
@@ -1,6 +1,14 @@
 import '../../factories.dart';
+import '../../../composites/composites.dart';
 import '../../../../data/usecases/usecases.dart';
 import '../../../../domain/usecases/usecases.dart';
 
-LoadSurveys makeLoadsurveys() => RemoteLoadSurveys(
+LoadSurveys makeRemoteLoadsurveys() => RemoteLoadSurveys(
     httpClient: makeAuthorizedHttpAdapter(), url: makeAPIUrl("surveys"));
+
+LoadSurveys makeLocalLoadsurveys() =>
+    LocalLoadSurveys(cacheStorage: makeLocalStorageAdapter());
+
+LoadSurveys makeRemoteLoadsurveysWithLocalFallback() =>
+    RemoteLoadSurveysWithLocalFallback(
+        remote: makeRemoteLoadsurveys(), local: makeLocalLoadsurveys());
