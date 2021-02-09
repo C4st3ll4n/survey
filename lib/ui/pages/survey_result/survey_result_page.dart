45610349531102544a052b03f970e3b87d3048a3
Pedro Henrique <p13dr0h@gmail.com>
2021-02-09 00:01:20 -0300

    refactor on survey result widget

--- lib/ui/pages/survey_result/survey_result_page.dart
+++ lib/ui/pages/survey_result/survey_result_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:survey/ui/components/reload_screen.dart';
+import 'package:survey/ui/pages/survey_result/components/components.dart';
 import '../../components/components.dart';
 import '../../helpers/helpers.dart';
 import '../pages.dart';
@@ -35,53 +36,34 @@ class SurveyResultPage extends StatelessWidget {
           return StreamBuilder<Object>(
             stream: presenter.surveyResultStream,
             builder: (context, snapshot) {
-  
               if (snapshot.hasError) {
-                return ReloadScreen(error: snapshot.error, reload: presenter.loadData,);
+                return ReloadScreen(
+                  error: snapshot.error, reload: presenter.loadData,);
               }
-              
-              return ListView.builder(
-              itemCount: (4 + 1),
-              itemBuilder: (contexto, indice) {
-                if (indice == 0) {
-                  return Container(
-                    padding: EdgeInsets.only(top: 40, bottom: 20, right: 20, left: 20),
-                    decoration: BoxDecoration(
-                      color: Theme.of(context).disabledColor.withAlpha(90),
-                    ),
-                      child: Text("Qual seu frame fav ?"));
-                } else {
-                  return Column(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.all(15),
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).backgroundColor,
-                        ),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Image.network("https://miro.medium.com/max/1000/1*ilC2Aqp5sZd1wi0CopD1Hw.png",
-                            width: 50,),
-                            Expanded(child: Padding(
-                              padding: const EdgeInsets.symmetric(horizontal: 10),
-                              child: Text("Vue JS ?", style: TextStyle(fontSize: 16),),
-                            )),
-                            Text("100 %", style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold, color: Theme.of(context).primaryColorDark),),
-                            Padding(
-                              padding: const EdgeInsets.only(left: 10),
-                              child: Icon(Icons.check_circle, color: Theme.of(context).highlightColor,),
-                            ),
-                          ],
-                        ),
-                      ),
-                      Divider(height: 1,),
-                    ],
-                  );
-                }
-              },
-        );
+              if (snapshot.hasData) {
+                return ListView.builder(
+                  itemCount: (4 + 1),
+                  itemBuilder: (contexto, indice) {
+                    if (indice == 0) {
+                      return Container(
+                          padding: EdgeInsets.only(
+                              top: 40, bottom: 20, right: 20, left: 20),
+                          decoration: BoxDecoration(
+                            color: Theme
+                                .of(context)
+                                .disabledColor
+                                .withAlpha(90),
+                          ),
+                          child: Text("Qual seu frame fav ?"));
+                    } else {
+                      return SurveyResult();
+                    }
+                  },
+                );
+              }
+              return SizedBox(width: 0,);
             }
+            
           );
         },
       ),
