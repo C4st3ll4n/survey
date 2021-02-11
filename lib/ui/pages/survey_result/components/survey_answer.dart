d70863529f76e2278afd158d48e099153fae02b4
Pedro Henrique <p13dr0h@gmail.com>
2021-02-11 00:53:05 -0300

    refactor on survey_answer image]

--- lib/ui/pages/survey_result/components/survey_answer.dart
+++ lib/ui/pages/survey_result/components/survey_answer.dart
@@ -7,48 +7,58 @@ class SurveyAnswer extends StatelessWidget {
   final SurveyAnswerViewModel viewModel;
 
   const SurveyAnswer({Key key, @required this.viewModel}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [
-        Container(
-          padding: const EdgeInsets.all(15),
-          decoration: BoxDecoration(
-            color: Theme.of(context).backgroundColor,
-          ),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              viewModel.image!=null ? Image.network(
-                "${viewModel.image}",
-                width: 50,
-              ) : SizedBox(width: 0,),
-              Expanded(
-                child: Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 10),
-                  child: Text(
-                    "${viewModel.answer}",
-                    style: TextStyle(fontSize: 16),
-                  ),
+      children: _buildItens(context),
+    );
+  }
+
+  List<Widget> _buildItens(BuildContext context) {
+    List<Widget> children = [
+      Container(
+        padding: const EdgeInsets.all(15),
+        decoration: BoxDecoration(
+          color: Theme.of(context).backgroundColor,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 10),
+                child: Text(
+                  "${viewModel.answer}",
+                  style: TextStyle(fontSize: 16),
                 ),
               ),
-              Text(
-                "${viewModel.percent}",
-                style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
-                    color: Theme.of(context).primaryColorDark),
-              ),
-              viewModel.isCurrentAnswer
-                  ? ActiveIcon()
-                  : DisableIcon(),
-            ],
-          ),
+            ),
+            Text(
+              "${viewModel.percent}",
+              style: TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                  color: Theme.of(context).primaryColorDark),
+            ),
+            viewModel.isCurrentAnswer ? ActiveIcon() : DisableIcon(),
+          ],
         ),
-        Divider(
-          height: 1,
+      ),
+      Divider(
+        height: 1,
+      ),
+    ];
+
+    if (viewModel.image != null) {
+      children.insert(
+        0,
+        Image.network(
+          "${viewModel.image}",
+          width: 50,
         ),
-      ],
-    );
+      );
+    }
+    return children;
   }
 }
