d2edcfe9123d2d22ba3b3a45dacc1084ed6fb729
Pedro Henrique <p13dr0h@gmail.com>
2021-02-11 00:48:38 -0300

    refactor on survey_result to survey header and survey answer

--- lib/ui/pages/survey_result/components/components.dart
+++ lib/ui/pages/survey_result/components/components.dart
@@ -2,3 +2,5 @@ export 'survey_result.dart';
 export 'survey_answer_view_model.dart';
 export 'survey_result_view_model.dart';
 export 'icons.dart';
+export 'survey_header.dart';
+export 'survey_answer.dart';
