46fd30c4fa99a895bef766fb37bde59740f948dd
Pedro Henrique <p13dr0h@gmail.com>
2021-02-13 01:09:17 -0300

    added the hideKeyboard to a mixin

--- lib/ui/pages/singup/signup_page.dart
+++ lib/ui/pages/singup/signup_page.dart
@@ -3,23 +3,18 @@ import 'package:get/route_manager.dart';
 import 'package:provider/provider.dart';
 import 'components/components.dart';
 import '../pages.dart';
+import '../../mixins/mixins.dart';
 import '../../helpers/helpers.dart';
 import '../../helpers/errors/errors.dart';
 import '../../components/components.dart';
 
-class SignUpPage extends StatelessWidget {
+class SignUpPage extends StatelessWidget with KeyboardManager{
   final SignUpPresenter presenter;
 
   const SignUpPage({Key key, this.presenter}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    void _hideKeyboard() {
-      final _currentFocus = FocusScope.of(context);
-      if (_currentFocus.hasPrimaryFocus) {
-        _currentFocus.unfocus();
-      }
-    }
 
     return Scaffold(
       body: Builder(
@@ -47,7 +42,7 @@ class SignUpPage extends StatelessWidget {
           });
 
           return GestureDetector(
-            onTap: _hideKeyboard,
+            onTap: ()=> hideKeyboard(context),
             child: SingleChildScrollView(
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.stretch,
