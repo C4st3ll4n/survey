9e244180084eb15c1bf3120f036db67447408006
Pedro Henrique <p13dr0h@gmail.com>
2021-02-14 05:36:23 -0300

    ensure composite returns local data when remote fails

--- lib/main/factories/usecases/load_survey_result_factory.dart
+++ lib/main/factories/usecases/load_survey_result_factory.dart
@@ -1,8 +1,18 @@
 import '../factories.dart';
+import '../../composites/composites.dart';
 import '../../../data/usecases/usecases.dart';
 import '../../../domain/usecases/usecases.dart';
 
-LoadSurveyResult makeRemoteLoadSurveyResult(String surveyID) {
-	return RemoteLoadSurveyResult(httpClient: makeAuthorizedHttpAdapter(),
-			url: makeAPIUrl("surveys/$surveyID/results"));
+LoadSurveyResult makeLoadSurveyResult(String surveyID) {
+  return RemoteLoadSurveyResultWithLocalFallback(
+      remote: _makeRemoteLoadSurveyResult(surveyID), local: _makeLocalLoadSurveyResult(surveyID));
 }
+
+LoadSurveyResult _makeRemoteLoadSurveyResult(String surveyId) =>
+    RemoteLoadSurveyResult(
+      httpClient: makeAuthorizedHttpAdapter(),
+      url: makeAPIUrl("surveys/$surveyId/results"),
+    );
+
+LoadSurveyResult _makeLocalLoadSurveyResult(String surveyId) =>
+    LocalLoadSurveyResult(cacheStorage: makeLocalStorageAdapter());//FIXME
