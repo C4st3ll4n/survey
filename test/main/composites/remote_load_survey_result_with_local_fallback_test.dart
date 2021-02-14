95171eca51771eff08abded6d62826b919f668ee
Pedro Henrique <p13dr0h@gmail.com>
2021-02-14 04:45:30 -0300

    ensure composite calls local save on loadBysurvey with remote date

--- test/main/composites/remote_load_survey_result_with_local_fallback_test.dart
+++ test/main/composites/remote_load_survey_result_with_local_fallback_test.dart
@@ -1,8 +1,9 @@
 import 'package:faker/faker.dart';
-import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 import 'package:survey/data/usecases/usecases.dart';
+import 'package:survey/domain/entities/entities.dart';
 import 'package:survey/main/composites/composites.dart';
+import 'package:test/test.dart';
 
 class RemoteSpy extends Mock implements RemoteLoadSurveyResult {}
 
@@ -13,17 +14,55 @@ void main() {
   RemoteLoadSurveyResultWithLocalFallback sut;
   RemoteLoadSurveyResult remoteSpy;
   LocalLoadSurveyResult localSpy;
-  
+  SurveyResultEntity mockedSurvey;
+
+  PostExpectation _mockLocalCall() => when(
+        localSpy.loadBySurvey(
+          surveyId: anyNamed("surveyId"),
+        ),
+      );
+
+  PostExpectation _mockRemoteCall() => when(
+        remoteSpy.loadBySurvey(
+          surveyId: anyNamed("surveyId"),
+        ),
+      );
+
+  void mockRemoteSuccess({SurveyResultEntity entity}) =>
+      _mockRemoteCall().thenAnswer((_) async => entity);
+
+  SurveyResultEntity mockSurveyResult() => SurveyResultEntity(
+        surveyId: faker.guid.guid(),
+        question: faker.lorem.sentence(),
+        answers: [
+          SurveyAnswerEntity(
+              answer: faker.lorem.sentence(),
+              percent: faker.randomGenerator.integer(100),
+              isCurrentAnswer: faker.randomGenerator.boolean()),
+        ],
+      );
+
   setUp(() {
     remoteSpy = RemoteSpy();
     localSpy = LocalSpy();
     surveyId = faker.guid.guid();
-    
-    sut = RemoteLoadSurveyResultWithLocalFallback(remote: remoteSpy, local: localSpy);
+
+    sut = RemoteLoadSurveyResultWithLocalFallback(
+        remote: remoteSpy, local: localSpy);
+
+    mockedSurvey = mockSurveyResult();
+    mockRemoteSuccess(entity: mockedSurvey);
   });
   test("Should call remote loadBySurvey", () async {
     await sut.loadBySurvey(surveyId: surveyId);
 
     verify(remoteSpy.loadBySurvey(surveyId: surveyId)).called(1);
   });
+
+  test("Should call local save with remote data", () async {
+    final resultEntity = await sut.loadBySurvey(surveyId: surveyId);
+    verify(localSpy.save(surveyId, resultEntity)).called(1);
+  });
+
+  test("Should return remote data", () async {});
 }
