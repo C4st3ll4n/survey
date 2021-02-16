dd0581708a34e59efadf7f3a20b0d5a80c5786a5
Pedro Henrique <p13dr0h@gmail.com>
2021-02-16 01:34:47 -0300

    ensure remoteSaveSurveyResult throws DomainErro if http fails"

--- test/data/usecases/save_survey_result/remote_save_survey_result_test.dart
+++ test/data/usecases/save_survey_result/remote_save_survey_result_test.dart
@@ -17,6 +17,7 @@ void main() {
         httpClient.request(
           url: anyNamed("url"),
           method: anyNamed("method"),
+          body: anyNamed("body"),
         ),
       );
 
@@ -70,6 +71,53 @@ void main() {
         body: {'answer':mockedAnswer}),
     );
   });
+
+  
+
+  /// TEST ON 404
+  test(
+    "Shoud throw an UnexpectedError if HttpClient returns 404",
+        () async {
+      _mockHttpError(HttpError.notFound);
+
+      final future = sut.save(answer: mockedAnswer);
+    
+      expect(
+        future,
+        throwsA(DomainError.unexpected),
+      );
+    },
+  );
+
+  /// TEST ON 500
+  test(
+    "Shoud throw an UnexpectedError if HttpClient returns 500",
+        () async {
+      _mockHttpError(HttpError.serverError);
+
+      final future = sut.save(answer: mockedAnswer);
+    
+      expect(
+        future,
+        throwsA(DomainError.unexpected),
+      );
+    },
+  );
+
+  /// TEST ON 401
+  test(
+    "Shoud throw an InvalidCrendential if HttpClient returns 403",
+        () async {
+      _mockHttpError(HttpError.forbidden);
+
+      final future = sut.save(answer: mockedAnswer);
+    
+      expect(
+        future,
+        throwsA(DomainError.accessDenied),
+      );
+    },
+  );
 
 }
 
