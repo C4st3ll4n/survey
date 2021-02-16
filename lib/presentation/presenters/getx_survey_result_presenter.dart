5414a2be912ec4e30dc984d9ac757aac68546930
Pedro Henrique <p13dr0h@gmail.com>
2021-02-16 04:26:13 -0300

    refactored code to reduce duplicated code and aply dRY

--- lib/presentation/presenters/getx_survey_result_presenter.dart
+++ lib/presentation/presenters/getx_survey_result_presenter.dart
@@ -1,30 +1,50 @@
 import 'package:get/get.dart';
 import 'package:meta/meta.dart';
+import 'package:survey/domain/entities/entities.dart';
 import '../mixins/mixins.dart';
 import '../../domain/helpers/domain_error.dart';
 import '../../ui/helpers/errors/errors.dart';
 import '../../domain/usecases/usecases.dart';
 import '../../ui/pages/pages.dart';
 
-class GetXSurveyResultPresenter extends GetxController  with SessionManager, LoadingManager implements SurveyResultPresenter {
-  GetXSurveyResultPresenter({@required this.loadSurveyResult, @required this.surveyId, @required this.saveSurveyResult, });
+class GetXSurveyResultPresenter extends GetxController
+    with SessionManager, LoadingManager
+    implements SurveyResultPresenter {
+  GetXSurveyResultPresenter({
+    @required this.loadSurveyResult,
+    @required this.surveyId,
+    @required this.saveSurveyResult,
+  });
 
   final LoadSurveyResult loadSurveyResult;
   final SaveSurveyResult saveSurveyResult;
   final String surveyId;
-  
+
   var _dataStream = Rx<SurveyResultViewModel>();
 
   @override
   Stream<bool> get isLoadingStream => loadingStream;
-  
+
   Stream<bool> get isSessionExpiredStream => sessionStream;
 
   @override
-  Future<void> loadData() async{
+  Future<void> loadData() async {
+    _process(() => loadSurveyResult.loadBySurvey(surveyId: surveyId));
+  }
+  
+  @override
+  Future<void> save({String answer}) async {
+    _process(() => saveSurveyResult.save(answer: answer));
+  }
+
+  @override
+  Stream<SurveyResultViewModel> get surveyResultStream =>
+      _dataStream.stream.distinct();
+
+  Future<void> _process(Future<SurveyResultEntity> action()) async {
     isLoading = true;
     try {
-      final entity = await loadSurveyResult.loadBySurvey(surveyId: surveyId);
+      final entity = await action();
       _dataStream.value = SurveyResultViewModel.fromEntity(entity);
     } on DomainError catch (e) {
       if (e == DomainError.accessDenied) {
@@ -33,31 +53,9 @@ class GetXSurveyResultPresenter extends GetxController  with SessionManager, Loa
         _dataStream.subject
             .addError(UIError.unexpected.description, StackTrace.empty);
       }
-    }
-    finally {
+    } finally {
       isLoading = false;
     }
   }
 
-  @override
-  Stream<SurveyResultViewModel> get surveyResultStream =>
-      _dataStream.stream.distinct();
-
-  @override
-  Future<void> save({String answer}) async {
-    isLoading = true;
-    try{
-      final entity = await saveSurveyResult.save(answer: answer);
-      _dataStream.value = SurveyResultViewModel.fromEntity(entity);
-    }on DomainError catch (e){
-      if (e == DomainError.accessDenied) {
-        isExpired = true;
-      } else {
-        _dataStream.subject
-            .addError(UIError.unexpected.description, StackTrace.empty);
-      }
-    }finally{
-      isLoading = false;
-    }
-  }
 }
